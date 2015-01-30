ead388c09a46f522febc68c63f387d432c6b8f22
Дарья Коваленко <dk@anahoret.com>
2015-01-30 11:52:19 +0200

    add button for move

---
+++ AMColorButton/Classes/AMMoveButton.h
@@ -0,0 +1,13 @@
+//
+//  AMMoveButton.h
+//  AMColorButton
+//
+//  Created by Daria Kovalenko on 1/30/15.
+//  Copyright (c) 2015 anadea. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AMMoveButton : NSButton
+
+@end
---
+++ AMColorButton/Classes/AMMoveButton.m
@@ -0,0 +1,39 @@
+//
+//  AMMoveButton.m
+//  AMColorButton
+//
+//  Created by Daria Kovalenko on 1/30/15.
+//  Copyright (c) 2015 anadea. All rights reserved.
+//
+
+#import "AMMoveButton.h"
+@interface AMMoveButton() {
+    BOOL shouldClick;
+}
+@end
+
+@implementation AMMoveButton
+
+#pragma mark - Mouse events
+- (void)mouseDown:(NSEvent *)theEvent {
+    [self.cell setHighlighted:YES];
+    shouldClick = YES;
+}
+
+- (void)mouseDragged:(NSEvent *)theEvent {
+    [self.superview mouseDragged:theEvent];
+    shouldClick = NO;
+}
+
+- (void)mouseUp:(NSEvent *)theEvent {
+    [self.cell setHighlighted:NO];
+    if (shouldClick) {
+        if (self.target && [self.target respondsToSelector:self.action]) {
+            IMP imp = [self.target methodForSelector:self.action];
+            void (*func)(id, SEL, id) = (void *)imp;
+            func(self.target, self.action, self);
+        }
+    }
+}
+
+@end
